1a2bfb3cfe42fe796279a252fb38d24be1047b4e
dunky-star <dunkygeoffrey39@gmail.com>
2024-01-09 20:30:33 -0600

    OrderConsumer in Stock Service

---
+++ stock-service/src/main/java/com/microservices/stockservice/kafka/OrderConsumer.java
@@ -0,0 +1,13 @@
+package com.microservices.stockservice.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+
+
+}
